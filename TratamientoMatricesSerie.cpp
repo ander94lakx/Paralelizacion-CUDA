eb67068420cd6e65374cd9327e3ef9621e1e1245
Ander Granado <andergranado@outlook.com>
2014-06-24 21:37:44 +0200

    Final Commit

--- TratamientoMatricesSerie.cpp
+++ TratamientoMatricesSerie.cpp
@@ -1,36 +1,41 @@
 #include "stdafx.h"
 
+// Includes C++
 #include <iostream> 
+
+// Includes C
 #include <time.h>
 
+// Namespaces
 using namespace std;
 
+// Declaraciones de las funciones
 float** rellenarMatriz(int tamano);
 void sumarMatrices(float **matriz1, float **matriz2, int tamano);
 void multiplicarMatrices(float **matriz1, float **matriz2, int tamano);
-void imprimirMatriz(float **matriz, int tamano);
 
 
-int _tmain(int argc, _TCHAR* argv[])
+int main(int argc, char** argv)
 {
-	clock_t inicio, fin, inicioGeneral, finGeneral;
+	clock_t inicio, fin, inicioGeneral, finGeneral; // Variables para calcular el tiempo
 
 	/*
-		srand() sirve para cambiar la semilla (numero) que se utiliza para generar los numeros
-		aleatorios. Al pasarle como parametro una llamada a la funcion time(), que obtiene el
-		tiempo actual del sistema, en cada ejecucion cada semilla (y en consecuencia los numeros
-		aleatorios generados) seran diferentes
-	*/
+	 *	srand() sirve para cambiar la semilla (numero) que se utiliza para generar los numeros
+	 *	aleatorios. Al pasarle como parametro una llamada a la funcion time(), que obtiene el
+	 *	tiempo actual del sistema, en cada ejecucion cada semilla (y en consecuencia los numeros
+	 *	aleatorios generados) seran diferentes siempre
+	 */
 	srand(time(NULL));
 	
+	// Constantes para los diferentes tamanos de matrices
 	const int tam1 = 10;
 	const int tam2 = 100;
 	const int tam3 = 300;
 	const int tam4 = 500;
 	const int tam5 = 700;
 	const int tam6 = 1000;
-	const int tam7 = 2000;
 	
+	// Se definen las variables que contendrán las matrices y se inicializan llamando al metodo rellenarMatriz()
 	cout << "INICIO DE LA CARGA DE MATRICES" << endl;
 	inicio = clock();
 	float **matriz1a = rellenarMatriz(tam1);
@@ -45,16 +50,21 @@ int _tmain(int argc, _TCHAR* argv[])
 	float **matriz5b = rellenarMatriz(tam5);
 	float **matriz6a = rellenarMatriz(tam6);
 	float **matriz6b = rellenarMatriz(tam6);
-	float **matriz7a = rellenarMatriz(tam7);
-	float **matriz7b = rellenarMatriz(tam7);
 	fin = clock();
 	cout << "Tiempo transcurrido: " << (fin-inicio)/(double)CLOCKS_PER_SEC << " segundos\n\n" << endl << endl << endl;
 
 	cout << "INICIO DE LAS PRUEBAS DE SUMAS Y PRODUCTOS DE MATRICES" << endl; 
 	cout << endl;
 
+	/*
+	 * La llamada al metodo clock() devuelve el instante de tiempo actual, en formato numerico double
+	 *
+	 * A lo largo de las pruebas es llama al metodo clock() para obtener el tiempo en el que se inica una operacion y
+	 * el tiempo en el que termina una operacion, para asi calculando la diferencia poder hallar el tiempo invertido en
+	 * realizar la operacion
+	 */
 	inicioGeneral = clock();
-
+	
 	// PRUEBAS 10x10
 
 	cout << endl;
@@ -170,84 +180,91 @@ int _tmain(int argc, _TCHAR* argv[])
 	cout << "Tiempo transcurrido en la ejecucion de las pruebas: " << endl;
 	cout << (finGeneral-inicioGeneral)/(double)CLOCKS_PER_SEC << " segundos\n\n" << endl;
 
-	getchar();
+	getchar(); // Espera a que se pulse una tecla para que la consola se quede abierta
 	return 0;
 }
 
 float** rellenarMatriz(int tamano)
 {	
 	/*
-		Para asignar la memoria necesaria a la matriz se utiliza primero un malloc que
-		reserva memoria para toda la matriz y luego se recorre cada fila para inicializar
-		cada array de float
-
-		Primero se reserva memoria para un array de arrays y despues para cada posicion de ese
-		array (que contiene un array de float) se reserva un tamano de memoria que es la
-		multiplicacion entreel numero de elementos y el tamaño que tiene el tipo de dato
-
-		Para ello se utiliza el parametro tamano
-	*/
+	 *	Para asignar la memoria necesaria a la matriz se utiliza primero un malloc que
+	 *	reserva memoria para toda la matriz y luego se recorre cada fila para inicializar
+	 *	cada array de float
+	 *
+	 *	Primero se reserva memoria para un array de arrays y despues para cada posicion de ese
+	 *	array (que contiene un array de float) se reserva un tamano de memoria que es la
+	 *	multiplicacion entre el numero de elementos y el tamaño que tiene el tipo de dato
+	 *
+	 *	Para ello se utiliza el parametro tamano
+	 */
 	float **matriz = (float**) malloc(tamano * sizeof(float*));
 
 	for(int k = 0; k < tamano; k++)
 		matriz[k] = (float*) malloc(tamano * sizeof(float));
 
+	/*
+	 *	Posteriormente se recorre la matriz para llenarla de numeros aleatorios
+	 */
 	for (int i = 0; i < tamano; ++i) 
 		for (int j = 0; j < tamano; ++j) 
 			/*
-				Como la funcion rand() genera un numero entero, para conseguir un numero decimal
-				se divide un numero aleatorio entre otro numero aleatorio para generar decimales,
-				sumandole 1 al divisor para que o pueda ser 0 y no falle en ejecucion
-
-				Tambien hay que hacer castings a tipo float a ambos operadores por separado para que la
-				division entre ellos devuelva float (dividir un entero entre un entero devuelve un
-				entero aunque el resultado real no sea un entero)
-			*/
+			 *	Como la funcion rand() genera un numero entero, para conseguir un numero decimal
+			 *	se divide un numero aleatorio entre otro numero aleatorio para generar decimales,
+			 *	sumandole 1 al divisor para que o pueda ser 0 y no falle en ejecucion
+			 *
+			 *	Tambien hay que hacer castings a tipo float a ambos operadores por separado para que la
+			 *	division entre ellos devuelva float (dividir un entero entre un entero devuelve un
+			 *	entero aunque el resultado real no sea un entero)
+			 */
 			matriz[i][j] = ((float) rand()) / ((float)(rand()+1));
-
-	/* Se puede llamar a un metodo imprimirMatriz para comprobar que realmente se estan 
-		introduciendo valores numericos en la matriz */
 
 	return matriz;
 }
 
+/*
+ * Metodo que calcula la suma de dos matrices
+ */
 void sumarMatrices(float **matriz1, float **matriz2, int tamano)
 {	
 	/*
-		Para asignar la memoria necesaria a la matriz se utiliza primero un malloc que
-		reserva memoria para toda la matriz y luego se recorre cada fila para inicializar
-		cada array de float
-
-		Primero se reserva memoria para un array de arrays y despues para cada posicion de ese
-		array (que contiene un array de float) se reserva un tamano de memoria que es la
-		multiplicacion entreel numero de elementos y el tamaño que tiene el tipo de dato
-
-		Para ello se utiliza el parametro tamano
-	*/
+	 *	Para asignar la memoria necesaria a la matriz se utiliza primero un malloc que
+	 *	reserva memoria para toda la matriz y luego se recorre cada fila para inicializar
+	 *	cada array de float
+	 *
+	 *	Primero se reserva memoria para un array de arrays y despues para cada posicion de ese
+	 *	array (que contiene un array de float) se reserva un tamano de memoria que es la
+	 *	multiplicacion entre el numero de elementos y el tamaño que tiene el tipo de dato
+	 *
+	 *	Para ello se utiliza el parametro tamano
+	 */
 	float **matrizFinal = (float**) malloc(tamano * sizeof(float*));
 
 	for(int k = 0; k < tamano; k++)
 		matrizFinal[k] = (float*) malloc(tamano * sizeof(float));
 
+	// Se recorre la matriz para realizar la suma para cada elemento
 	for (int i = 0; i < tamano; i++)
 		for (int j = 0; j < tamano; j++)
 			matrizFinal[i][j] = matriz1[i][j] + matriz2[i][j];
 
 }
 
-void multiplicarMatrices(float **matriz1, float **matriz2, int tamano)
+/*
+ * Metodo que calcula la multipilcacion de dos matrices
+ */
+ void multiplicarMatrices(float **matriz1, float **matriz2, int tamano)
 {	
 	/*
-		Para asignar la memoria necesaria a la matriz se utiliza primero un malloc que
-		reserva memoria para toda la matriz y luego se recorre cada fila para inicializar
-		cada array de float
-
-		Primero se reserva memoria para un array de arrays y despues para cada posicion de ese
-		array (que contiene un array de float) se reserva un tamano de memoria que es la
-		multiplicacion entreel numero de elementos y el tamaño que tiene el tipo de dato
-
-		Para ello se utiliza el parametro tamano
-	*/
+	 *	Para asignar la memoria necesaria a la matriz se utiliza primero un malloc que
+	 *	reserva memoria para toda la matriz y luego se recorre cada fila para inicializar
+	 *	cada array de float
+	 *
+	 *	Primero se reserva memoria para un array de arrays y despues para cada posicion de ese
+	 *	array (que contiene un array de float) se reserva un tamano de memoria que es la
+	 *	multiplicacion entre el numero de elementos y el tamaño que tiene el tipo de dato
+	 *
+	 *	Para ello se utiliza el parametro tamano
+	 */
 	float **matrizFinal = (float**) malloc(tamano * sizeof(float*));
 
 	for(int m = 0; m < tamano; m++)
@@ -257,18 +274,9 @@ void multiplicarMatrices(float **matriz1, float **matriz2, int tamano)
 		for(int n = 0; n < tamano; n++)
 			matrizFinal[l][n] = (float) 0;
 
+	// Se recorre la matriz para realizar la operacion de multiplicar para cada elemento
 	for (int i = 0; i < tamano; i++)
 		for (int j = 0; j < tamano; j++)
 			for (int k = 0; k < tamano; k++)
 				matrizFinal[i][j] += matriz1[i][k] * matriz2[k][j];
-}
-
-void imprimirMatriz(float **matriz, int tamano)
-{
-	for(int i = 0; i < tamano; i++) {
-		for(int j = 0; j < tamano; j++) {
-			cout << matriz[i][j] << " ";
-		}
-		cout << endl;
-	}
 }
